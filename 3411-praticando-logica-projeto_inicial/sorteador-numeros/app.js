443c270269cc0ab57f0726a238fa364e44e7dc49
Felipe de Souza Araujo <felipearaujoptc@gmail.com>
2024-10-10 19:59:54 -0300

    finalizado sorteador de numeros

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,9 +1,23 @@
 function sortear(){
-    let qualidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
-
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+    //Fazer o Alert funcionar
+    let numero = obterNumeroAleatorio(de,ate);
+    if (quantidade > 0){
+        for ( let i = 0; i < quantidade; i++){
+            let numero = obterNumeroAleatorio(de, ate);
+            alert(numero);
+        }
+    }else{
+        alert("Porfavor insira uma quantidade valida")
+    }
 }
+//Calculo que pega o número aleatorio
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Colocar números sorteados
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">${numero}</label>`
